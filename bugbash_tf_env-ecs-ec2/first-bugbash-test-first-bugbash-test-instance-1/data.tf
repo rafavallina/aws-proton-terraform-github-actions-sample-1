20a111602e3f50b94b6e225ed5eed60ac78c1aac
sbx_user1051 <sbx_user1051@169.254.156.73>
2022-05-02 23:29:21 +0000

    Update requested for Proton - service/first-bugbash-test/service-instance/first-bugbash-test-instance-1

---
+++ bugbash_tf_env-ecs-ec2/first-bugbash-test-first-bugbash-test-instance-1/data.tf
@@ -0,0 +1,23 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:987544922694:service/first-bugbash-test/service-instance/first-bugbash-test-instance-1
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+variable "task_sizes" {
+  default = {
+    x-small = { cpu = 256, memory = 512 }
+    small   = { cpu = 512, memory = 1024 }
+    medium  = { cpu = 1024, memory = 2048 }
+    large   = { cpu = 2048, memory = 4096 }
+    x-large = { cpu = 4096, memory = 8192 }
+  }
+}
